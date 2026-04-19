576b0784ff4f4cf24dc20cc60cd26b91a54b425e
whskdb <dangphuongify@gmail.com>
2026-04-19 18:40:22 +0700

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,152 @@
+const friends = [
+  {
+    name: "Thái Khắc Duy",
+    password: "khacduy", 
+    image: "images/khacduy.jpg",
+    letter: `hi duy
+adgfkjsdfgjhbcjhgf
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Trần Tùng Linh",
+    password: "tling", 
+    image: "images/tunglinh.jpg",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Phan Trung Minh",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Đức Việt",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Vũ Bảo Sơn",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Phạm Hoàng An",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Đức Anh",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Trần Hữu Đăng Trường",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Thạch Thảo",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Ngô Phương Linh",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Quang Trung",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Đỗ Hoàng Lâm",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Trọng Phúc Hưng",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Nam Khánh",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Bùi Quang Khải",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Vũ Nguyễn Tiến Anh",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Minh Khang",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Thế Phong",
+    password: "tling", 
+    image: "https://via.placeholder.com/300",
+    letter: `hi tlinh
+ya brownies tastes good
+asjdgfkasjdfgkjsadgfgf 😍😍😍`
+  },
+  {
+    name: "Nguyễn Xuân Phúc",
+    password: "xp",
+    image: "https://via.placeholder.com/300",
+    letter: "Dear xp, you are awesome..."
+  }
+];
